e0e352e96a6ad66cc1ebb56dfd48cb9d110387cd
KiritoCun <123577900+JarikMalyavka@users.noreply.github.com>
2025-01-20 10:48:19 +0300

    Update ok.lua

--- yes2/ok.lua
+++ yes2/ok.lua
@@ -2,3 +2,17 @@ manky_flip = Kok
 print( manky_flip )
 print("манки")
 print("print" .. "манки")
+
+
+local myName = "Jeff" 
+print( "Hello, " .. myName .. "!\nHow are you today?\nI'm feeling great." )
+// выйдет вот это
+// Hello, Jeff!
+// How are you today?
+// I'm feeling great.
+
+
+print( [[Hello, Jeff!
+How are you today?
+I'm feeling great.]] )
+// тоже самое будет

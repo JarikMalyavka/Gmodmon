a4287b90276bc46feca0c4d9b8198739865f9458
KiritoCun <123577900+JarikMalyavka@users.noreply.github.com>
2025-01-21 11:10:40 +0300

    Update manky.lua

--- yes2/manky.lua
+++ yes2/manky.lua
@@ -1,7 +1,8 @@
-Mnakysok = 20
-
-[1] = 1
-[2] = 2
-[3] = 3
-[4] = "MMMMManky"
-[5] = "kraska"
+for i=1,8 do --8 being the width of the backpack.
+    ply.Inv.Backpack[i] = {}
+end
+for k,v in pairs(ply.Inv.Backpack)do
+  for i=1,4 do --4 being the height of the backpack.
+      ply.Inv.Backpack[k][i] = false --False is a placeholder here. We'll overwrite that later.
+  end
+end
